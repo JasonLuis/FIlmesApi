22d491c8f2d82822829a4655c66b79ecd9c97a97
JasonLuis <jason20luis@gmail.com>
2023-12-13 01:35:28 -0300

    Adicionar arquivos de projeto.

---
+++ FIlmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,50 @@
+﻿using FIlmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIlmesApi.Controllers;
+
+/*
+ Para que essa classe seja um controlador e esteja hábil a lidar com requisições de usuários,
+ precisamos adicionar alguns elementos. A primeira delas são as anotações
+ [ApiController] e [Route],
+ antes da definição da classe:
+ */
+[ApiController] 
+[Route("[controller]")]
+public class FilmeController: ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost] // Requisição POST
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+
+    /*
+         A principal finalidade do IEnumerable é fornecer uma maneira padrão de iterar
+         sobre os elementos de uma coleção sem expor os detalhes internos da 
+         implementação da coleção. Ele define um método chamado GetEnumerator(),
+         que retorna um objeto que implementa a interface IEnumerator.
+         A interface IEnumerator possui métodos como MoveNext() para avançar
+         para o próximo elemento e Current para obter o elemento atual da iteração.
+     */
+
+    [HttpGet] // Requisição GET
+    public IEnumerable<Filme> RecuperaFilme()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")] // Requisição GET que recebe uma queryParam
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
---
+++ FIlmesApi/Models/Filme.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIlmesApi.Models;
+
+/*
+Validação de dados: data annotations
+
+Required: torna o dado obrigatório
+MaxLength: valida a quantidade máxima de caracteres
+Range:  limita o intervalo inferior e superior para valores numéricos
+ErrorMessage: Personaliza a mensagem de erro
+
+StringLength:  limita o tamanho de caracteres de uma string
+ */
+
+public class Filme
+{
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    [MaxLength(200, ErrorMessage = "O título do filme não pode exceder 200 caracteres")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
